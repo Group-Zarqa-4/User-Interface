d1d0502460342babf106efcb2b7b769939e1f1b3
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-12-11 15:29:06 +0200

    Jafar Review

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import ReviewCards from "../Components/Reviews/ReviewCard";
 function Reviews() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="p-5">
+        <ReviewCards />
+      </div>
+    </div>
+  );
 }
 
 export default Reviews;
